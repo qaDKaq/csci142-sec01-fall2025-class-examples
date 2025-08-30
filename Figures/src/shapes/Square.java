434931e61def9dc6403f686bbc8865a3ff3e6458
daniel-plante <daniel.plante@gmail.com>
2025-08-29 21:02:08 -0400

    Added initial broken Figures project

---
+++ Figures/src/shapes/Square.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Square {
+	private float sideLength;
+	private float area;
+	
+	public Square() {
+		sideLength = 1.0f;
+		area = 1.0f;
+	}
+	
+	public Square(float sideLength) {
+		sideLength = sideLength;
+	}
+
+}
